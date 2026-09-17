678227cc7541248d8d9a5a3db41b9a4a35c4dfad
Govind Dhawale <gdhawale@xtsworld.in>
2026-09-17 12:03:56 +0530

    Implement admin login changes

--- node_modules/xts-admin/dist/services/admin/src/repositories/role-menu.repository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.findAll = findAll;
-async function findAll() {
-    return [];
-}
-//# sourceMappingURL=role-menu.repository.js.map
--- node_modules/xts-admin/src/graphql/typeDefs/role-permissions.typeDefs.ts
+++
@@ -1,9 +0,0 @@
-export default `
-  type RolePermissions {
-    id: Int!
-  }
-
-  extend type Query {
-    rolePermissionsList: [RolePermissions]
-  }
-`;
